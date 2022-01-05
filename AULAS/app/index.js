1b02d17532874feae9e7416ea9d27595b85c86ec
Vinícius Dabian <vinicius_dabian@hotmail.com>
2022-01-05 19:55:46 -0300

    Summary

--- AULAS/app/index.js
+++ AULAS/app/index.js
@@ -23,11 +23,19 @@ const histogram = new prom.Histogram({
     buckets: [0.1, 0.2, 0.3, 0.4, 0.5]
 });
 
+const summary = new prom.Summary({
+    name: 'aula_summary_request_time_seconds',
+    help: 'Tempo de resposta da API',
+    percentiles: [0.5, 0.9, 0.99]
+});
+
 app.get('/', function(req, res) {
     counter.labels('200').inc();
     counter.labels('300').inc();
-    gauge.set(100*Math.random());
-    histogram.observe(Math.random());
+    gauge.set(100 * Math.random());
+    const tempo = Math.random();
+    histogram.observe(tempo);
+    summary.observe(tempo);
     res.send('Hello World!');
 });
 
